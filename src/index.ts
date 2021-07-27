2e396a74e892e3ff0f4df74c8bee3b1ab84be65c
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-27 11:27:12 +0530

    Use regexp test method instead of exec

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,11 @@ import convertToRegExp from './convertToRegExp';
 function esGlob(str: string, pattern: string): boolean {
   let flag = false;
   const negate = pattern[0] === '!';
-  const regexStr = convertToRegExp(negate ? pattern.substring(1) : pattern);
+  const regExpStr = convertToRegExp(negate ? pattern.substring(1) : pattern);
 
   try {
-    const regexp = new RegExp(regexStr);
-    const result = regexp.exec(str);
-
-    if (result) {
-      flag = str.length === result[0].length;
-    }
+    const regExp = new RegExp(`^${regExpStr}$`);
+    flag = regExp.test(str);
 
     return negate ? !flag : flag;
   } catch (error) {
